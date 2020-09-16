a3c059f9bc3fc66d3766b0ebdc5cb3554327a06d
Christian Westhoff <westhoffchristian1@gmail.com>
2020-09-16 13:09:59 +0200

    Added linting config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apollo'
+import { useApollo } from '../graphql/apollo'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const apolloClient = useApollo(pageProps.initialApolloState)
